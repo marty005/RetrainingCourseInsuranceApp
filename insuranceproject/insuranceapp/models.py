2a7fe3cabfb5268d63d7d04571935731b8a4dd21
Sterbis <sterbam2@gmail.com>
2023-08-21 00:08:56 +0200

    InsuranceApp part 2

--- insuranceproject/insuranceapp/models.py
+++ insuranceproject/insuranceapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.urls import reverse
 
 
-class PersonBaseInfo(models.Model):
+class Person(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     birth_date = models.DateField()
@@ -24,11 +24,15 @@ class PersonBaseInfo(models.Model):
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
     
+    @property
+    def personal_identification_number_as_string(self):
+        return f"{str(self.personal_identification_number)[:6]}/{str(self.personal_identification_number)[6:]}"
+    
     def __str__(self):
         return self.full_name
 
 
-class Policyholder(PersonBaseInfo):
+class Policyholder(Person):
     """
     Pojistník
     """
@@ -41,13 +45,13 @@ class Policyholder(PersonBaseInfo):
         return reverse("insuranceapp:policyholder-detail", kwargs={"pk": self.id})
 
 
-class Insured(PersonBaseInfo):
+class Insured(Person):
     """
     Pojištěný
     """
 
 
-class Beneficiary(PersonBaseInfo):
+class Beneficiary(Person):
     """
     Oprávněná osoba
     """
@@ -56,29 +60,65 @@ class Beneficiary(PersonBaseInfo):
         verbose_name_plural = "beneficiaries"
 
 
-class InsuraceBaseInfo(models.Model):
+class Insurace(models.Model):
+    insurance_product_name = "Insurance"
+
     policyholder = models.ForeignKey(Policyholder, on_delete=models.CASCADE)
     # contract_id = models.IntegerField(unique=True)
-    contract_conclusion_date = models.DateField()
+    contract_date = models.DateField()
     valid_from_date = models.DateField()
     valid_to_date = models.DateField()
 
     class Meta:
         abstract = True
 
+    def __str__(self):
+        return self.insurance_product_name
+    
+    @classmethod
+    def get_view_name_prefix(cls):
+        return cls.insurance_product_name.replace(' ', '').lower()
+    
+    def get_absolute_url(self):
+        return self.get_detail_url()
 
-class LifeInsurance(InsuraceBaseInfo):
+    def get_detail_url(self):
+        return reverse(f"insuranceapp:{self.__class__.get_view_name_prefix()}-detail", kwargs={"pk": self.id})
+
+    def get_update_url(self):
+        return reverse(f"insuranceapp:{self.__class__.get_view_name_prefix()}-update", kwargs={"pk": self.id})
+
+    def get_delete_url(self):
+        return reverse(f"insuranceapp:{self.__class__.get_view_name_prefix()}-delete", kwargs={"pk": self.id})
+
+
+class LifeInsurance(Insurace):
+    """
+    Životní pojištění
+    """
+    insurance_product_name = "Life Insurance"
+
     sum_assured = models.IntegerField() # Pojistná částka
-    insured = models.ForeignKey(Insured, on_delete=models.CASCADE, blank=True, null=True)
-    beneficiary = models.ForeignKey(Beneficiary, on_delete=models.CASCADE, blank=True, null=True)
+    insured = models.ForeignKey(Insured, on_delete=models.SET_NULL, blank=True, null=True)
+    beneficiary = models.ForeignKey(Beneficiary, on_delete=models.SET_NULL, blank=True, null=True)
+
+
+class HomeInsurance(Insurace):
+    """
+    Pojištění domácnosti
+    """
+    insurance_product_name = "Home Insurance"
+
+    sum_assured = models.IntegerField() # Pojistná částka
+    street_and_reference_number = models.CharField(max_length=50, blank=True, null=True)
+    city = models.CharField(max_length=50, blank=True, null=True)
+    postal_code = models.IntegerField(blank=True, null=True)
 
     @property
-    def insurance_product_name(self):
-        return "Life Insurance"
-
-    def get_absolute_url(self):
-        return reverse("insuranceapp:lifeinsurance-detail", kwargs={"pk": self.id})
+    def address(self):
+        return f"{self.street_and_reference_number} {self.city}, {self.postal_code}"
 
 
 class EInsuranceProduct(Enum):
+    HOME_INSURANCE = HomeInsurance
     LIFE_INSURANCE = LifeInsurance
--- insuranceproject/insuranceapp/views.py
+++ insuranceproject/insuranceapp/views.py
@@ -1,6 +1,7 @@
 import datetime
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import (
     CreateView,
@@ -10,10 +11,20 @@ from django.views.generic import (
     UpdateView,
 )
 
-from .models import EInsuranceProduct, LifeInsurance, Policyholder
+from .models import (
+    EInsuranceProduct,
+    HomeInsurance,
+    LifeInsurance,
+    Policyholder,
+)
 
-def index(request):
-    return HttpResponse("Insurance App!!!")
+
+def base(request):
+    return render(request, "insuranceapp/base.html")
+
+
+def insurance_product_select(request, policyholder_id):
+    return HttpResponseRedirect(reverse(f"insuranceapp:{request.POST['view_name_prefix']}-create", kwargs={"policyholder_id": policyholder_id}))
 
 
 class PolicyholderModelMixin:
@@ -43,9 +54,11 @@ class PolicyholderDetailView(PolicyholderModelMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        insurance_product_list = [insurance_product.value for insurance_product in EInsuranceProduct]
+        context["insurance_product_list"] = insurance_product_list
         policyholder_isurance_list = []
-        for insurance_product in EInsuranceProduct:
-            policyholder_isurance_list.extend(insurance_product.value.objects.filter(policyholder=self.kwargs["pk"]))
+        for insurance_product in insurance_product_list:
+            policyholder_isurance_list.extend(insurance_product.objects.filter(policyholder=self.kwargs["pk"]))
         context["policyholder_isurance_list"] = policyholder_isurance_list
         return context
 
@@ -62,6 +75,35 @@ class PolicyholderUpdateView(PolicyholderModelMixin, PolicyholderFormFieldsMixin
 
 class PolicyholderDeleteView(PolicyholderModelMixin, DeleteView):
     success_url = reverse_lazy("insuranceapp:policyholder-list")
+
+
+class InsuranceCreateView(CreateView):
+    template_name = "insuranceapp/insurance_form.html"
+
+    def get_initial(self):
+        initial = super().get_initial()
+        initial["policyholder"] = self.kwargs["policyholder_id"]
+        return initial
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["insurance_product_name"] = self.model.insurance_product_name
+        return context
+
+    def form_valid(self, form):
+        form.instance.contract_date = datetime.date.today()
+        return super().form_valid(form)
+    
+
+class InsuranceUpdateView(UpdateView):
+    template_name = "insuranceapp/insurance_form.html"
+    
+
+class InsuranceDeleteView(DeleteView):
+    template_name = "insuranceapp/insurance_confirm_delete.html"
+
+    def get_success_url(self):
+        return reverse("insuranceapp:policyholder-detail", kwargs={"pk": self.object.policyholder.id})
 
 
 class LifeInsuranceModelMixin:
@@ -82,21 +124,44 @@ class LifeInsuranceDetailView(LifeInsuranceModelMixin, DetailView):
     pass
 
 
-class LifeInsuranceCreateView(LifeInsuranceModelMixin, LifeInsuranceFormFieldsMixin, CreateView):
-    def get_initial(self):
-        initial = super().get_initial()
-        initial["policyholder"] = self.kwargs["policyholder_id"]
-        return initial
-
-    def form_valid(self, form):
-        form.instance.contract_conclusion_date = datetime.date.today()
-        return super().form_valid(form)
-
-
-class LifeInsuranceUpdateView(LifeInsuranceModelMixin, LifeInsuranceFormFieldsMixin, UpdateView):
+class LifeInsuranceCreateView(LifeInsuranceModelMixin, LifeInsuranceFormFieldsMixin, InsuranceCreateView):
     pass
 
 
-class LifeInsuranceDeleteView(LifeInsuranceModelMixin, DeleteView):
-    def get_success_url(self):
-        return reverse("insuranceapp:policyholder-detail", kwargs={"pk": self.object.policyholder.id})
+class LifeInsuranceUpdateView(LifeInsuranceModelMixin, LifeInsuranceFormFieldsMixin, InsuranceUpdateView):
+    pass
+
+
+class LifeInsuranceDeleteView(LifeInsuranceModelMixin, InsuranceDeleteView):
+    pass
+
+
+class HomeInsuranceModelMixin:
+    model = HomeInsurance
+
+
+class HomeInsuranceFormFieldsMixin:
+    fields = [
+        "policyholder",
+        "valid_from_date",
+        "valid_to_date",
+        "sum_assured",
+        "street_and_reference_number",
+        "city",
+        "postal_code",
+    ]
+
+class HomeInsuranceDetailView(HomeInsuranceModelMixin, DetailView):
+    pass
+
+
+class HomeInsuranceCreateView(HomeInsuranceModelMixin, HomeInsuranceFormFieldsMixin, InsuranceCreateView):
+    pass
+
+
+class HomeInsuranceUpdateView(HomeInsuranceModelMixin, HomeInsuranceFormFieldsMixin, InsuranceUpdateView):
+    pass
+
+
+class HomeInsuranceDeleteView(HomeInsuranceModelMixin, InsuranceDeleteView):
+    pass
